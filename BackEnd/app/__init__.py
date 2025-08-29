36fe49e366e10d46e652eb1702f0f6ea98754b07
4ndres90 <venierandres@gmail.com>
2025-08-29 10:23:45 -0300

    Initial backend project structure

---
+++ BackEnd/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+from .api.routes import api_bp
+
+def create_app():
+    app = Flask(__name__)
+    
+    #Enables cross-origin resource sharing (get requests from Frontend)
+    CORS(app)
+    
+    #Register a blueprint on the application (routes) 
+    app.register_blueprint(api_bp)
+    
+    return app
