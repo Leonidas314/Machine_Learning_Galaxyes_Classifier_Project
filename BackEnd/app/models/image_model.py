73d38a6b72cac52b98b2f38e26adf972520249ab
Lautaro Budin <lautaro_budin@hotmail.com>
2025-09-08 15:38:49 -0300

    Refactor ImageModel

--- BackEnd/app/models/image_model.py
+++ BackEnd/app/models/image_model.py
@@ -1,8 +1,16 @@
 from datetime import datetime
-from app.db import db
+from ..db import db
 
 class ImageModel(db.Model):
+    __tablename__ = "images"
+
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False) #Almacena la Url de la imagen
+    filename = db.Column(db.String(255), nullable=False)
     category = db.Column(db.String(50), nullable=False)
-    creation_date = db.Column(db.DateTime, default=datetime.utcnow) #¿Es necesario fijar la hora de Argentina?
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, filename: str, category: str, creation_date: datetime | None = None) -> None:
+        super().__init__()
+        self.filename = filename
+        self.category = category
+        self.creation_date = creation_date or datetime.utcnow()
